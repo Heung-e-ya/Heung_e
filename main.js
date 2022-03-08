603f7d5075eb78f8f71a3db5abcda115b64fb933
heung <hoonun@gmail.com>
2022-03-08 22:29:55 +0900

    check

---
+++ main.js
@@ -0,0 +1,40 @@
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark');
+        
+        
+    } else {
+        navbar.classList.remove('navbar--dark');
+        
+    }
+}); 
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null){
+        return;
+    }
+    navbarMenu.classList.remove('open');
+    scrollIntoView(link);
+});
+
+// Nabar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+const homeContact = document.querySelector('#contact');
+homeContactBtn.addEventListener('click', () => {
+    homeContact.scrollIntoView('#contact')
+});
+
